4fa1c4f1696f281a21c85f782cc2335c11c3353e
faycel sassi <faycel.sassi@esen.tn>
2022-05-27 08:34:08 +0100

    Projet Symfony

---
+++ src/Entity/Question.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\QuestionRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=QuestionRepository::class)
+ */
+class Question
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="text", nullable=true)
+     */
+    private $question;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $Titre;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getQuestion(): ?string
+    {
+        return $this->question;
+    }
+
+    public function setQuestion(?string $question): self
+    {
+        $this->question = $question;
+
+        return $this;
+    }
+
+    public function getTitre(): ?string
+    {
+        return $this->Titre;
+    }
+
+    public function setTitre(string $Titre): self
+    {
+        $this->Titre = $Titre;
+
+        return $this;
+    }
+}
